f921b52a4058a6e26384d9ab5cecde0b67ae40ca
mathew180 <mathewasuku400@gmail.com>
2021-01-07 05:30:06 -0800

    todoList

--- todo.js
+++ todo.js
@@ -4,7 +4,6 @@ const todoButton = document.querySelector('.todoButton');
 const todoList = document.querySelector('.todoList');
 
 //AddEventListener
-document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', delOption);
 
@@ -23,10 +22,6 @@ function addTodo(event){
     todoItem.classList.add("todoItem");
     todoDiv.appendChild(todoItem);
 
-
-    
-    //ADDTODO TO LOCALSTORAGE
-    saveLocalTodos(todoInput.value)
   
    //Delete button
      const delTodo = document.createElement('button');
@@ -49,47 +44,6 @@ const todo = item.parentElement
   
 }
 
-function saveLocalTodos(todo){
-    let todos;
-    if(localStorage.getItem('todos') === null){
-        todos = [];
-    }else{
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-    todos.push(todo);
-    localStorage.setItem("todos",JSON.stringify(todos));
-}
 
-function getTodos(){
 
-    let todos;
- 
-    if(localStorage.getItem('todos') === null){
-        todos = [];
-    }else{
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
-  
-    todos.forEach(function(todo){
-     //TODO DIV
-     const todoDiv = document.createElement('div');
-     todoDiv.classList.add("todo");
- 
-    //Create List Item
-     const todoItem = document.createElement('li');
-     todoItem.innerText = todo;
-     todoItem.classList.add("todoItem");
-     todoDiv.appendChild(todoItem);
- 
-   
-    //Delete button
-      const delTodo = document.createElement('button');
-      delTodo.innerHTML = `x`;
-      delTodo.classList.add("delTodo");
-      todoDiv.appendChild(delTodo);
- 
-      //APPEND TO LIST
-      todoList.appendChild(todoDiv);
-    });
-}
 
